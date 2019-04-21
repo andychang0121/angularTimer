add78a80a27934354dcf3c9549f93656bb704290
安迪學長 <andy_chang_0121@hotmail.com>
2019-04-21 22:38:01 +0800

    Tidy Codes

--- src/app/timer-main/timer-main.component.ts
+++ src/app/timer-main/timer-main.component.ts
@@ -15,7 +15,7 @@ export class TimerMainComponent {
   countDownClock = '';
   sessionName = 'timer';
   clock = '';
-
+  handleEvents = ['click', 'mousemove', 'mouseover', 'mouseout', 'keyup', 'keydown', 'keypress', 'scroll'];
   /*
   * 重置倒數計時器
   * @param o: TimerMainComponent
@@ -30,8 +30,12 @@ export class TimerMainComponent {
     return s;
   }
   constructor() {
+    this.onEvent(this.handleEvents);
     this.countDownNumber = this.initSession(this.sessionName, this.countDownNumber);
     this.setLocalSession(this.sessionName, this.countDownNumber.toString());
+
+    setInterval(() => { this.clock = this.getDate(); }, this.intervalTime);
+
     this.clockTimer = setInterval(() => {
       this.main();
     }, this.intervalTime);
@@ -48,10 +52,10 @@ export class TimerMainComponent {
     const initState = !localStorage.hasOwnProperty(name) || isNaN(storeNumber);
     const retuNumber = initState ? value : storeNumber;
     console.log('init ->', retuNumber);
-    // console.log(Object.getOwnPropertyNames(TimerMainComponent.prototype));
+    // console.log(Object.getOwnPropertyNames(HeroTimerComponent.prototype));
     return retuNumber;
   }
-  setLocalSession(name: string, value: string) {
+  setLocalSession(name: string = '', value: string) {
     localStorage.setItem(name, value);
   }
 
@@ -61,9 +65,9 @@ export class TimerMainComponent {
 
   getDate(date: Date = null) {
     date = date === null ? new Date() : date;
-    const yearStr = this.padLeft(date.getFullYear(), 4);
-    const monthStr = this.padLeft(date.getMonth(), 2);
-    const dayStr = this.padLeft(date.getDay(), 2);
+    // const yearStr = this.padLeft(date.getFullYear(), 4);
+    // const monthStr = this.padLeft(date.getMonth(), 2);
+    // const dayStr = this.padLeft(date.getDay(), 2);
     const hourStr = this.padLeft(date.getHours(), 2);
     const minsStr = this.padLeft(date.getMinutes(), 2);
     const secondStr = this.padLeft(date.getSeconds(), 2);
@@ -72,11 +76,11 @@ export class TimerMainComponent {
   }
 
   main() {
-    this.clock = this.getDate();
     this.countDownNumber--;
     if (this.countDownNumber < 0) {
       setTimeout(() => {
         clearInterval(this.clockTimer);
+        this.offEvent(this.handleEvents);
         console.log('Close Interval:', this.clockTimer);
       }, 500);
     } else {
@@ -84,28 +88,19 @@ export class TimerMainComponent {
       this.countDownClock = this.getCountDownClock(this.countDownNumber);
     }
   }
-
-  @HostListener('window:beforeunload', ['$event']) unloadHandler(event: Event) {
-    console.log('Processing beforeunload...', this.defaultValue);
-    localStorage.removeItem(this.sessionName);
-    this.setLocalSession('defaultValue', this.defaultValue.toString());
-    this.reset(this);
+  onEvent(events: string[]) {
+    const target = document.body;
+    for (const e of events) {
+      target.addEventListener(e, () => {
+        this.reset(this);
+      });
+    }
   }
-
-  @HostListener('document:click', ['$event']) private onClick(event: any) {
-    console.log('click');
-    this.reset(this);
+  offEvent(events: string[]) {
+    const target = document.body;
+    for (const e of events) {
+      target.removeEventListener(e, () => {});
+    }
   }
-  @HostListener('document:keypress', ['$event']) private onKeypress(event: any) {
-    console.log('keypress');
-    this.reset(this);
-  }
-  @HostListener('document:mouseover', ['$event']) private onMouseover(event: any) {
-    console.log('mouseover');
-    this.reset(this);
-  }
-  @HostListener('document:scroll', ['$event']) private onScroll(event: any) {
-    console.log('Scroll');
-    this.reset(this);
-  }
+  @HostListener('window:beforeunload', ['$event']) unloadHandler(event: Event) {}
 }
